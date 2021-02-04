31da448b8e0503eca4590c60452d3e8221610860
Daniel Williams <mail@daniel-williams.co.uk>
2021-02-04 15:13:14 +0000

    Updated the heron model interface.

--- gravitic/models.py
+++ gravitic/models.py
@@ -3,7 +3,7 @@ Gravitic blocks for models.
 """
 
 from heron.models.gw import HofTSurrogate, BBHNonSpinSurrogate
-from heron.models.torchbased import CUDAModel
+from heron.models.torchbased import HeronCUDA
 import heron.models.georgebased
 import george
 import numpy as np
@@ -20,38 +20,43 @@ from heron.models.torchbased import train, HeronCUDAMix
 from gravitic.block import Block, blockmap
 
 class HeronTrainingBlock(Block):
-    """ 
+    """
     The training block for heron models.
     """
     def __init__(self, specification, pipeline=None):
         super().__init__(specification, pipeline=pipeline)
         self.ready = False
-        
+
         self.inputs = 1
         self.outputs = 1
-        
+
         self.model = None
         self.pipeline = pipeline
 
         self.output_files = {"training_data": self.input_data['training_data'],
-                             "weights": "model_state.pth"}
-        
-        
+                             "weights":  f"{specification['name']}.pth"}
+
     def build(self):
         self.model = HeronCUDAMix(specification=self.specification, **self.input_data)
         self.ready = True
         
     def output(self):
         if self.ready:
-            out = {}
-            out['training data'] = training_data
+            out = self.output_files
             out['specification'] = self.specification
-            out['weights'] = "model_state.pth"
             return out
         return None
-        
-    def run(self):
+
+    def run(self, **kwargs):
         if not self.ready:
             self.build()
-        train(self.model, iterations=self.specification['iterations'])
+        if self.status_flag == "ready":
+            # Models need to be trained before the mean can be returned
+            train(self.model, iterations=self.specification['iterations'])
+            # TODO update the specification
+            # self.specification
+
+        return self.model.mean(times=kwargs['times'],
+                               p=kwargs['p'])
+
 blockmap.register_block("data.waveform.heron", HeronTrainingBlock)
